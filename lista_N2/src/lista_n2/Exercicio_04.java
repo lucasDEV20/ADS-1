157d6c096c83a138b3d4f6e0f59fd70d090a357f
lucas araujo da silva <55209015+lucasDEV20@users.noreply.github.com>
2019-10-07 22:49:28 -0300

    Add files via upload

--- lista_N2/src/lista_n2/Exercicio_04.java
+++ lista_N2/src/lista_n2/Exercicio_04.java
@@ -1,46 +1,72 @@
 /*
-4. Escreva um programa que leia o nome e a idade de vários usuários, e imprima a
-seguinte mensagem “O usuário *nome* tem *anos* anos de vida”. O programa deverá
-continuar executando enquanto o usuário informar que deseja continuar.
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
+package aulaads_1;
 
-package lista_n2;
 import java.util.Scanner;
+
+/**
+ *
+ * @author aluno
+ */
 public class Exercicio_04 {
-    
+
     public static void main(String[] args) {
-        Scanner entrada = new Scanner (System.in);
-        String nome = null , resposta  ;
-        int idade = 0, quant = 0, dados = 0 ;
+        Scanner entrada = new Scanner(System.in);
+        String nome,nome_menor,nome_maior;
+        int opc, idade, cont = 0, soma_idades = 0 , idade_menor = 0,  idade_maior = 999 ;
         //entrada / processamento de execuçao do progrma 
-        System.out.println();
-        System.out.println("==================================================");
-        System.out.println("Quantas pessoas vao ser entrevistadas\n");
-        quant = entrada.nextInt();
-        while(dados <= quant){
-        System.out.println();
-        System.out.println("==================================================");
-        System.out.printf("\nPor favor digite o nome do usuario\n");
-        nome = entrada.next();
-        System.out.printf("\nDigite a idade do usuario \n");
-        idade = entrada.nextInt();
-        dados++;
-        System.out.println();
-        //processamento       
-        
-            /*ACHAR UMA VALIDAÇAO PRA ESSE BLOCO , CASO O USUARIO
-       DIGITE O NOME EM MAIUSCULO , OU A PRINEIRA LETRA
-        ELE IRA EXPRESSAR ISSO
-        */
-         
-       
-        
+
+        do {
+            System.out.println();
+            System.out.println("==================================================");
+            System.out.printf("\nPor favor digite o nome do usuario\n");
+            nome = entrada.next();
+
+            System.out.printf("\nDigite a idade do usuario \n");
+            idade = entrada.nextInt();
+
+            if (idade == 0) {
+                System.out.println("fim da coleta de idades");
+            } else if (idade < 0) {
+                System.out.println("idade invalida");
+            } else {
+               
+                System.out.println("O usuário , " + nome + " tem " + idade + " anos de vida");
+                System.out.println("==================================================");
+                System.out.println(); 
+
+                //acumlamdo as idades
+                 cont++;
+                 soma_idades = idade + soma_idades;
+                if (idade > idade_maior) {
+                    idade_maior = idade;
+                    nome_maior = nome;
+                }
+                if (idade > idade_maior) {
+                    idade_menor = idade;
+                    nome_menor = nome;
+                }
+                System.out.println();
+                System.out.println("=====Etatisticas======");
+                System.out.println("o nome do usuario e ");
+                System.out.println("a quantidade de pessoas entrevistadas e "+ cont);
+                System.out.println("a media das idades e "+soma_idades);
+                System.out.println("a idade maior e "+idade_maior);
+                System.out.println("a idade menor e" +idade_menor);
+                
+                
+            }
+
+            //envia para o coletor de lixo do java
+            entrada.nextLine();
+
+        } while (idade != 0);
+
         //saida
-        System.out.println("O usuário , "+nome+" tem "+idade+" anos de vida");
-        System.out.println("==================================================");
-        System.out.println();
-  }
-        }
-        
+       
     }
 
+}
