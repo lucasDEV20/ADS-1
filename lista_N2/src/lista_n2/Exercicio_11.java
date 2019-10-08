69c202955d78c71ee452e93d8547eee91c2fb287
lucas araujo da silva <55209015+lucasDEV20@users.noreply.github.com>
2019-10-08 20:33:59 -0300

    Add files via upload

--- lista_N2/src/lista_n2/Exercicio_11.java
+++ lista_N2/src/lista_n2/Exercicio_11.java
@@ -10,18 +10,17 @@ public class Exercicio_11 {
 
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
-        int cont = 0, num_inteiro = 0;
-        double num_double = 0 , media;
-        do {
-            System.out.println("INSIRA UM NUMERO INTEIRO");
-            num_inteiro = entrada.nextInt();
-            System.out.println("INSIRA UM NUMERO REAL (DOUBLE)");
-            num_double = entrada.nextDouble(); 
-            media = num_inteiro + num_double /2;
-            cont++;
-            System.out.println("para sair do programa digite 0 ");
-            System.out.println("A MEDIA DOS NUMEROS FORNECIDOS E "+media);
-        } while (num_inteiro > 0 || num_double > 0);
-        System.out.println("fim da execuçao do programa ");
+        int  num_inteiro ;
+        double num_double , media;
+
+        System.out.println("INSIRA UM NUMERO INTEIRO");
+        num_inteiro = entrada.nextInt();
+        System.out.println("INSIRA UM NUMERO REAL (DOUBLE)");
+        num_double = entrada.nextDouble();
+        media = (num_inteiro + num_double )/ 2;
+      
+       
+        System.out.println("A MEDIA DOS NUMEROS FORNECIDOS É " + media);
+
     }
 }
