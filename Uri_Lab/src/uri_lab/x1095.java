e1e535272c8a65477367a1cc761cb7ab558ff57a
lucas araujo da silva <55209015+lucasDEV20@users.noreply.github.com>
2019-11-06 22:38:42 -0300

    Add files via upload

---
+++ Uri_Lab/src/uri_lab/x1095.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+
+I=4 J=55
+
+...
+*/
+package lista_uri;
+
+public class x1095 {
+    public static void main(String[] args) {
+        for (int i = 1; i == 1; i++) {
+            for (int j = 60; j == 60; j++) {
+                System.out.println("I="+i+" "+"J="+j);
+                for (int k = 4; k == 4; k++) {
+                    for (int l = 55; l == 55; l++) {
+                       System.out.println("I="+k+" "+"J="+l);
+                        for (int m = 7; m == 7; m++) {
+                            for (int n = 50; n == 50; n++) {
+                                System.out.println("I="+m+" "+"J="+n);
+                                    System.out.println("...");
+                                      for (int o = 0; o == 0; o++) {
+                                          System.out.println("I=?"+" "+"J="+o);
+                                }
+                            }
+                        }
+                    }
+                }
+                
+            }
+        }
+    }
+    
+}
