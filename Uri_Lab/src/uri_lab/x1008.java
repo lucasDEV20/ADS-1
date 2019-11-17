7253d1b7afa69f65483bea3430cd9a3af39df0ce
lucas araujo da silva <55209015+lucasDEV20@users.noreply.github.com>
2019-11-17 19:43:24 -0300

    Create x1008.java

---
+++ Uri_Lab/src/uri_lab/x1008.java
@@ -0,0 +1,15 @@
+class Main {
+  public static void main(String[] args) {
+Scanner entrada = new Scanner (System.in);
+int num_funcionario , horas , salario;
+
+num_funcionario = entrada.nextInt();
+horas = entrada.nextInt();
+salario = entrada.nextInt();
+
+System.out.printf("NUMBER = %d ",num_funcionario);
+System.out.printf("SALARY = U$ %.3f ",salario);
+
+
+  }
+}
